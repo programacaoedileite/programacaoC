88b1108a7f31194715100de5b9c0f6739bc91eef
programacaoedileite <edileusaleite42ads@gmail.com>
2025-04-09 14:15:16 +0000

    Codigo completo

---
+++ EstruturaFor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero, i;
+
+    printf("Digite um número para calcularmos a tabuada...\n");
+    scanf("%d", &numero);
+
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d x %d = %d \n", numero, i, i * numero);
+    }
+    return 0;
+}
